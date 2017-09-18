1004c521a77b872128b57a05a431eba3f6e5e926
Roman Gich <roman.gich@gmail.com>
2017-09-18 23:10:16 +0300

    init

---
+++ src/Config.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Gich\Config;
+
+/**
+ * Description of Config
+ *
+ * @author Roman Gich <roman.gich@gmail.com>
+ */
+class Config {
+    
+    
+    public function get($param) {
+        
+    }
+    
+    public function set($param) {
+        
+    }
+    
+}
---
+++ src/ConfigLoader.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Gich\Config;
+
+/**
+ * Description of ConfigLoader
+ *
+ * @author Roman Gich <roman.gich@gmail.com>
+ */
+class ConfigLoader {
+    
+    public function __construct($dir) {
+        
+    }
+    
+    /**
+     * @return Config
+     */
+    public function load() {
+        
+    }
+    
+    /**
+     * @return Config
+     */
+    public function loadDir($dir) {
+        
+    }
+    
+}
